9183047cde9dad7bccbb6d96fc30b0b603cb40df
kounet95 <85202452+kounet95@users.noreply.github.com>
2024-09-24 14:12:45 -0400

    r

---
+++ rajout/src/test/java/kounettechnologie/kounet/rajout/RajoutApplicationTests.java
@@ -0,0 +1,13 @@
+package kounettechnologie.kounet.rajout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RajoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
